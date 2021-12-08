692e18401393d57c6a9ab8950b89abad5e864bed
Artem <noktigula@gmail.com>
2021-12-08 20:41:28 +0100

    Alarm list Compose components moved to a separate file

--- app/src/main/java/com/happyday/android/MainActivity.kt
+++ app/src/main/java/com/happyday/android/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.google.android.material.timepicker.MaterialTimePicker
+import com.happyday.android.compose.ListContent
 import com.happyday.android.repository.*
 import com.happyday.android.ui.theme.HappyDayTheme
 import com.happyday.android.utils.loge
@@ -38,104 +39,35 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.getAlarms().observe(this) { allAlarms ->
             setContent {
-                HappyDayTheme {
-                    // A surface container using the 'background' color from the theme
-                    Surface(color = MaterialTheme.colors.background) {
-                        Column {
-                            AlarmsList(Modifier.weight(1f), this@MainActivity, allAlarms)
-                            Button(onClick = {
-                                val picker = MaterialTimePicker.Builder().build()
-                                picker.addOnPositiveButtonClickListener {
-                                    loge("Selected ${picker.hour}:${picker.minute}")
-                                    val id = UUID.randomUUID()
-                                    val alarm = AlarmModel(
-                                        id,
-                                        "",
-                                        true,
-                                        null,
-                                        true,
-                                        picker.hour,
-                                        picker.minute,
-                                        alarms = mapOf(
+                ListContent(activity = this, allAlarms = allAlarms) {
+                    val picker = MaterialTimePicker.Builder().build()
+                    picker.addOnPositiveButtonClickListener {
+                        loge("Selected ${picker.hour}:${picker.minute}")
+                        val id = UUID.randomUUID()
+                        val alarm = AlarmModel(
+                            id,
+                            "",
+                            true,
+                            null,
+                            true,
+                            picker.hour,
+                            picker.minute,
+                            alarms = mapOf(
 //                                            Weekday.Mon to SingleAlarm(id, Weekday.Mon, picker.hour, picker.minute),
 //                                            Weekday.Tue to SingleAlarm(id, Weekday.Tue, picker.hour, picker.minute),
-                                            Weekday.Wed to SingleAlarm(id, Weekday.Wed, picker.hour, picker.minute),
+                                Weekday.Wed to SingleAlarm(id, Weekday.Wed, picker.hour, picker.minute),
 //                                            Weekday.Thu to SingleAlarm(id, Weekday.Thu, picker.hour, picker.minute),
 //                                            Weekday.Fri to SingleAlarm(id, Weekday.Fri, picker.hour, picker.minute),
 //                                            Weekday.Sat to SingleAlarm(id, Weekday.Sat, picker.hour, picker.minute),
 //                                            Weekday.Sun to SingleAlarm(id, Weekday.Sun, picker.hour, picker.minute),
-                                        )
-                                    )
-                                    viewModel.addAlarm(alarm)
-                                }
-                                picker.show(this@MainActivity.supportFragmentManager, "picker")
-                            }) {
-                                Text("Schedule")
-                            }
-                        }
+                            )
+                        )
+                        viewModel.addAlarm(alarm)
                     }
+                    picker.show(this@MainActivity.supportFragmentManager, "picker")
                 }
             }
         }
 
     }
 }
-
-@Composable
-fun AlarmsList(modifier: Modifier, context: Context, data: List<AlarmModel>) {
-    LazyColumn(modifier = modifier) {
-        items(data) { item ->
-            AlarmRow(context, item)
-        }
-    }
-}
-
-@Composable
-fun AlarmRow(context: Context, item: AlarmModel) {
-    Card {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = item.readableTime(context))
-            Column (horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(text = item.title)
-                WeekdaysSelector(selectedWeekdays = item.alarms.keys)
-            }
-            Switch(checked = item.enabled, onCheckedChange = {checked -> /*TODO*/})
-        }
-    }
-}
-
-@Composable
-fun WeekdaysSelector(selectedWeekdays: Set<Weekday>) {
-    Row {
-        Weekday.values().map { WeekdayBox(it.name, selectedWeekdays.contains(it)) { status ->
-            //TODO update status in viewmodel
-        } }
-    }
-}
-
-@Composable
-fun WeekdayBox(title: String, selected: Boolean, onSelected: (Boolean)->Unit) {
-    return Text(text = title, color = if (selected) Color.Blue else Color.Black)
-}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    HappyDayTheme {
-//        AlarmsList(listOf(
-//                Alarm(AlarmModel(UUID.randomUUID(), "Test", 18 hours 35.minutes(), setOf(Weekday.Mon, Weekday.Tue), enabled = true)) { hrs, min ->
-//                    "18:35"
-//                },
-//        Alarm(AlarmModel(UUID.randomUUID(), "Test 2", 8 hours 0.minutes(), setOf(Weekday.Wed, Weekday.Fri), enabled = true)) { hrs, min ->
-//            "08:00"
-//        },
-//        Alarm(AlarmModel(UUID.randomUUID(), "Test 3", 7 hours 30.minutes(), setOf(Weekday.Sat, Weekday.Sun), enabled = true))  { hrs, min ->
-//            "07:30"
-//        },
-//        ))
-//    }
-//}
---
+++ app/src/main/java/com/happyday/android/compose/alarmListComponents.kt
@@ -0,0 +1,95 @@
+package com.happyday.android.compose
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.happyday.android.repository.AlarmModel
+import com.happyday.android.repository.AllAlarms
+import com.happyday.android.repository.Weekday
+import com.happyday.android.ui.theme.HappyDayTheme
+import com.happyday.android.utils.readableTime
+
+@Composable
+fun ListContent(activity: AppCompatActivity, allAlarms: AllAlarms, onAddAlarm:()->Unit) {
+    HappyDayTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Column {
+                AlarmsList(Modifier.weight(1f), activity, allAlarms)
+                Button(onClick = onAddAlarm) {
+                    Text("Schedule")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AlarmsList(modifier: Modifier, context: Context, data: List<AlarmModel>) {
+    LazyColumn(modifier = modifier) {
+        items(data) { item ->
+            AlarmRow(context, item)
+        }
+    }
+}
+
+@Composable
+fun AlarmRow(context: Context, item: AlarmModel) {
+    Card {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = item.readableTime(context))
+            Column (horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = item.title)
+                WeekdaysSelector(selectedWeekdays = item.alarms.keys)
+            }
+            Switch(checked = item.enabled, onCheckedChange = {checked -> /*TODO*/})
+        }
+    }
+}
+
+@Composable
+fun WeekdaysSelector(selectedWeekdays: Set<Weekday>) {
+    Row {
+        Weekday.values().map { WeekdayBox(it.name, selectedWeekdays.contains(it)) { status ->
+            //TODO update status in viewmodel
+        } }
+    }
+}
+
+@Composable
+fun WeekdayBox(title: String, selected: Boolean, onSelected: (Boolean)->Unit) {
+    return Text(text = title, color = if (selected) Color.Blue else Color.Black)
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    HappyDayTheme {
+//        AlarmsList(listOf(
+//                Alarm(AlarmModel(UUID.randomUUID(), "Test", 18 hours 35.minutes(), setOf(Weekday.Mon, Weekday.Tue), enabled = true)) { hrs, min ->
+//                    "18:35"
+//                },
+//        Alarm(AlarmModel(UUID.randomUUID(), "Test 2", 8 hours 0.minutes(), setOf(Weekday.Wed, Weekday.Fri), enabled = true)) { hrs, min ->
+//            "08:00"
+//        },
+//        Alarm(AlarmModel(UUID.randomUUID(), "Test 3", 7 hours 30.minutes(), setOf(Weekday.Sat, Weekday.Sun), enabled = true))  { hrs, min ->
+//            "07:30"
+//        },
+//        ))
+//    }
+//}
+
